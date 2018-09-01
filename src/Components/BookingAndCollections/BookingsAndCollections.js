ae53c7097bb7e2d6e35f6bc7cf01cf12b60512ba
Isambard Wheeler-artwell <isambardwheeler-artwell@isambards-air.home>
2018-09-01 19:29:56 +0100

    Created Component Folder

---
+++ src/Components/BookingAndCollections/BookingsAndCollections.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { Grid } from 'semantic-react-ui';
+
+class BookingsAndCollections extends Component {
+
+    render() {
+
+        return(
+            <Grid>
+
+                <Header> Collection Call </Header>
+
+            </Grid>
+        );
+    }
+}
