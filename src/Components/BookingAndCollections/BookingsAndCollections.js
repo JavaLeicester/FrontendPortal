07e02a124277208a9c8eb65feb567ff30516fbcb
Isambard Wheeler-artwell <isambardwheeler-artwell@host-115-6.eduroam-local.wifi.le.ac.uk>
2018-09-02 11:27:32 +0100

    Completed Basic layout for BookingAndCollections Form

--- src/Components/BookingAndCollections/BookingsAndCollections.js
+++ src/Components/BookingAndCollections/BookingsAndCollections.js
@@ -1,15 +1,128 @@
+import './BookingsAndCollections.css';
+import { Piece } from '../Piece/Piece';
 import React, { Component } from 'react';
-import { Grid } from 'semantic-react-ui';
+import { Grid, Form, Segment, Input, Header, Divider, Select, TextArea, Button } from 'semantic-ui-react';
 
-class BookingsAndCollections extends Component {
+export class BookingsAndCollections extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            isLooselyPacked: [{key: "yes", text: "yes"}, {key: "no", text: "no"}],
+        };
+
+    }
 
     render() {
 
+        var { isLooselyPacked } = this.state;
+
         return(
-            <Grid>
+            <Grid className='one column center aligned blue' container>
+                <Grid.Column width={12} class="form">
+                    <Form className="blue">
+                        <Segment className='raised small'>
+                            Collection Call
+                        </Segment>
 
-                <Header> Collection Call </Header>
+                        <Header>
+                            Collection Data
+                        </Header>
 
+                        <Form.Group widths='equal' className='package'>
+                            <Form.Field
+                                control={Input}
+                                name='customerName'
+                                placeholder='customerName'
+                                label="Customer name"
+
+                            />
+                        </Form.Group>
+
+                        <Form.Group widths='equal' className='package'>
+                            <Form.Field
+                                control={Input}
+                                name='houseNumber'
+                                placeholder='houseNumber'
+                                label="House number"
+                            />
+                        </Form.Group>
+
+                        <Form.Group widths='equal' className='package'>
+                            <Form.Field
+                                control={Input}
+                                name='street'
+                                placeholder='street'
+                                label="street"
+                            />
+                        </Form.Group>
+
+                        <Form.Group widths='equal' className='package'>
+                            <Form.Field
+                                control={Input}
+                                name='postcode'
+                                placeholder='postcode'
+                                label="postcode"
+                            />
+                        </Form.Group>
+
+                        <Form.Group widths='equal' className='package'>
+                            <Form.Field
+                                control={Input}
+                                name='City'
+                                placeholder='City'
+                                label="City"
+                            />
+                        </Form.Group>
+
+                        <Form.Group widths='equal' className='package'>
+                            <Form.Field
+                                control={ TextArea }
+                                name="specialInstructions"
+                                placeholder="Special Instructions For Delivery"
+                                label="Special instructions for Delivery"
+                            />
+                        </Form.Group>
+
+                        <Divider />
+
+                        <Header> Parcel Information </Header>
+
+                        <Form.Group widths='equal' className='package'>
+                            <Form.Field
+                                control={ TextArea }
+                                label="General description of Goods"
+                                placeholder="General description of Goods"
+                            />
+                        </Form.Group>
+
+                        <Form.Group widths='equal' className='package'>
+                            <Form.Radio
+                                label='Tick, if parcel loosely packed!'
+                            />
+                        </Form.Group>
+
+                        <Form.Group widths='equal' className='package'>
+                            <Form.Radio
+                                label='Tick, if parcel/s contain hazardous goods (e.g. perfumes)'
+                            />
+                        </Form.Group>
+
+                        <Divider />
+
+                        <Grid.Row>
+                            <Piece />
+                        </Grid.Row>
+
+                        <Divider className="black" />
+
+                        <Button
+                            content='Confirm Collection'
+                        />
+
+                    </Form>
+
+                </Grid.Column>
             </Grid>
         );
     }
