48941035742215b79fbd70c0229e5aaffbb82212
Isambard Wheeler-artwell <isambardwheeler-artwell@Isambards-Air.home>
2018-09-25 12:40:57 +0100

    Added Account dir

---
+++ src/Components/Accounts/LoginForm/LoginForm.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Form, Grid } from 'semantic-ui-react';
+
+class LoginForm extends React.Component {
+
+
+    render() {
+
+        return(
+            <Grid>
+                <Header> Apex Login </Header>
+
+            </Grid>
+        );
+
+    }
+
+
+}
