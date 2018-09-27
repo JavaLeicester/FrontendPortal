0b3c9f64ab0fa480d98a39279f2b795741f07c13
Isambard Wheeler-artwell <isambardwheeler-artwell@Isambards-Air.home>
2018-09-27 13:59:48 +0100

    Changed port to 8080

--- src/Components/ApiClient/ApiClient.js
+++ src/Components/ApiClient/ApiClient.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 function getServerBaseUrl() {
      // just local development at the moment
-     return `http://46.101.34.160:9001`;
+     return `http://46.101.34.160:8080`;
      // return `http://localhost:8080`;
 }
 
