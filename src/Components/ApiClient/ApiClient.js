0474f07f110cf1528a4a1ef87562f08f34120d24
Isambard Wheeler-artwell <isambardwheeler-artwell@host-201-138.eduroam-local.wifi.le.ac.uk>
2018-10-05 10:13:36 +0100

    Changed port

--- src/Components/ApiClient/ApiClient.js
+++ src/Components/ApiClient/ApiClient.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 function getServerBaseUrl() {
      // just local development at the moment
-    // return `http://46.101.34.160:9000`;
-     return `http://46.101.34.160:8080`;
+     // return `http://46.101.34.160:9000`;
+     return `http://46.101.34.160:443`;
 }
 
 export const SERVER_BASE_URL = getServerBaseUrl();
