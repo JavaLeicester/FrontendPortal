8b4a256fb530c45876997f93337321e8827205af
Isambard Wheeler-artwell <isambardwheeler-artwell@Isambards-Air.home>
2018-10-16 12:23:44 +0100

    Basic layout for login from

--- src/Components/Accounts/LoginForm/LoginForm.js
+++ src/Components/Accounts/LoginForm/LoginForm.js
@@ -1,27 +1,56 @@
+import './LoginForm.css';
 import React from 'react';
-import { Form, Grid, Segment } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
+import { Grid, Button, Form, Segment, Container, Header} from 'semantic-ui-react';
 
 class LoginForm extends React.Component {
 
-
-    render() {
-
-        return(
-            <Grid textAlgin='center' verticalAlgin='middle'>
-                <Grid.Column>
-                    <Header> Apex Login </Header>
-                    <Segment stacked>
-                        <Form>
-                            <Form.Input/>
-                            <Form.Input/>
-                            <Form.Button> Login </Form.Button>
-                        </Form>
-                    </Segment>
-                </Grid.Column>
-            </Grid>
-        );
-
+    constructor(props) {
+        super(props);
+        this.state = {
+            userName: '',
+            password: '',
+        };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
 
+    handleChange() {
+
+
+    }
+
+    handleSubmit() {
+
+
+    }
+
+    render() {
+        const { handleSubmit, handleChange } = this;
+        return (
+
+                <Grid className='login-form' textAlign='center' verticalAlign='middle'>
+                    <Grid.Column className='login_grid'>
+                        <Form>
+                            <Header className='ui teal center aligned header'>
+                                Login into your account
+                            </Header>
+                                <Form
+                                    size='large'
+                                    onSubmit={handleSubmit}>
+                                    <Form.Group widths='equal'>
+                                        <Form.Input fluid label='username' placeholder='username'/>
+                                        <Form.Input fluid label='password' placeholder='password'/>
+                                    </Form.Group>
+                                </Form>
+                            <Button type='submit'>Submit</Button>
+                        </Form>
+                    </Grid.Column>
+                </Grid>
+
+        );
+    }
 }
+
+export default LoginForm;
